a1813254555786ae289c307207ea098cd2d3de56
Darkenight07 <samueldynamo11@gmail.com>
2023-11-13 23:02:20 +0100

    Logica hecha

--- src/Botones.java
+++ src/Botones.java
@@ -3,9 +3,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 public class Botones {
 
-    int puntos;
-    int contadorClicker;
-    int contadorClickerSupremo;
+    public int puntos;
+    public int contadorClicker;
+    public int contadorClickerSupremo;
+    private int contadorClickerSupremoNum;
+
     public Botones(int puntos, int contadorClicker, int contadorClickerSupremo) {
         this.puntos = puntos;
         this.contadorClicker = contadorClicker;
@@ -33,32 +35,52 @@ public class Botones {
         clicker1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (puntos >= 50) {
-                    puntos -= 50;
-                    contadorClicker += 5;
+                if (contadorClicker >= 5) {
+                    JOptionPane.showMessageDialog(null, "No puedes comprar mas clickers, tienes " + contadorClicker + " clickers, solo se pueden comprar 5");
+                } else if (puntos >= 10) {
+                    puntos -= 10;
+                    contadorClicker += 1;
                 } else {
                     JOptionPane.showMessageDialog(null, "No tienes suficientes puntos");
                 }
             }
         });
-
     }
 
     public void BotonClicker2(JFrame frame) {
         JButton clicker2 = new JButton("Comprar 1 ClickerSupremo");
         clicker2.setBounds(330, 350, 145, 40);
         frame.add(clicker2);
-    }
 
+
+        clicker2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (contadorClickerSupremoNum >= 5) {
+                    JOptionPane.showMessageDialog(null, "No puedes comprar mas clickers supremos, tienes " + contadorClickerSupremoNum + " clickers supremos, solo se pueden comprar 5");
+                } else if (puntos >= 100) {
+                    puntos -= 100;
+                    contadorClickerSupremo += 5;
+                    contadorClickerSupremoNum++;
+                } else {
+                    JOptionPane.showMessageDialog(null, "No tienes suficientes puntos");
+                }
+            }
+        });
+    }
     public int getContadorClicker() {
         return contadorClicker;
     }
 
     public int getContadorClickerSupremo() {
-        return contadorClickerSupremo;
+        return contadorClickerSupremoNum;
     }
 
     public int getPuntos() {
         return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 import javax.swing.*;
 
 public class Main {
-    public static int puntos = 0;
     public static void main(String[] args) {
-        Botones botones = new Botones(1, 0, 0);
+        Botones botones = new Botones(0, 0, 0);
 
         JFrame frame = new JFrame("Cookie Clicker: | Puntos: ");
         frame.setResizable(false);
@@ -19,13 +18,12 @@ public class Main {
         Thread bucle = new Thread(() -> {
             while (true) {
                 botones.BotonGalleta(frame);
-                puntos = botones.getPuntos();
                 botones.BotonClicker1(frame);
                 botones.BotonClicker2(frame);
 
-                verificadorBotonesPuntos(botones);
-                frame.setTitle("Cookie Clicker: | Puntos: " + puntos);
-                labelPuntos.setText("Puntos: " + puntos);
+                actualizar(botones);
+                frame.setTitle("Cookie Clicker: | Puntos: " + botones.getPuntos());
+                labelPuntos.setText("Puntos: " + botones.getPuntos());
             }
         });
         bucle.start();
@@ -33,14 +31,24 @@ public class Main {
         frame.setVisible(true);
     }
 
-    public static void verificadorBotonesPuntos(Botones botones) {
-        if (botones.getContadorClicker() >= 5) {
-            puntos += botones.getContadorClicker();
+    public static void actualizar(Botones botones) {
+        if (botones.getContadorClicker() >= 1) {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            botones.setPuntos(botones.getPuntos() + botones.getContadorClicker());
+        }
+
+        if (botones.getContadorClickerSupremo() >= 5) {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            botones.setPuntos(botones.getPuntos() + botones.getContadorClickerSupremo());
         }
     }
-    public static void actualizar() {
-        
-    }
-
 
 }
